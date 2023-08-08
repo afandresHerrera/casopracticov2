0e1d4e81f4cc4dbab585af0082a5cfd759e1bdd8
Andres Herrera <afandres19@gmail.com>
2023-08-08 18:53:37 -0500

    Primer intento de context menu

--- src/app/modules/products/products.component.ts
+++ src/app/modules/products/products.component.ts
@@ -2,13 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
+interface ShowProduct extends Product {
+  showcontext?: boolean;
+}
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  products: Product[] = [];
+  openContext?: number | null;
+  products: ShowProduct[] = [];
 
   constructor(
     private productsService: ProductService
@@ -19,9 +24,52 @@ export class ProductsComponent implements OnInit {
   }
 
   getProducts() {
-    this.productsService.getProducts().subscribe(res => {
-      console.log(res);
-      this.products = res;
-    })
+    this.productsService.getProducts().subscribe(response => {
+      this.products = response.map((res: ShowProduct) => {
+        return {
+          ...res,
+          showcontext: false
+        }
+      });
+      console.log(this.products);
+    });
+  }
+
+  showContext(idx: number) {
+    console.log(this.openContext, idx);
+    if (!!this.openContext || this.openContext === 0) {
+      this.hideContext(this.openContext);
+    }
+
+    // deberia funcionar
+    if (this.openContext === idx) {
+      this.hideContext(idx);
+    } else {
+      this.products[idx].showcontext = true;
+      this.openContext = idx;
+    }
+  }
+
+  hideContext(idx: number) {
+    this.products[idx].showcontext = false;
+    this.openContext = null;
+  }
+
+  editProduct(element: ShowProduct) {
+    if (!!this.openContext || this.openContext === 0) {
+      this.hideContext(this.openContext);
+    }
+    console.log('se editaera el elemento :' + JSON.stringify(element));
+  }
+
+  deleteProduct(element: ShowProduct) {
+    if (!!this.openContext || this.openContext === 0) {
+      this.hideContext(this.openContext);
+    }
+    console.log('se elminar el elemento :' + JSON.stringify(element));
+  }
+
+  test() {
+    console.log("mouseout!");
   }
 }
