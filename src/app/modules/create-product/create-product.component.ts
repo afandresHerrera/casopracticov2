63af82a7b1a6c0b364ddfd6b6ddc4a825ccff6c9
Andres Herrera <afandres19@gmail.com>
2023-08-08 22:31:55 -0500

    Se añade logica para la fecha

--- src/app/modules/create-product/create-product.component.ts
+++ src/app/modules/create-product/create-product.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -15,7 +16,6 @@ export class CreateProductComponent implements OnInit {
     this.initForm();
   }
 
-
   initForm() {
     this.productoForm = new FormGroup({
       id: new FormControl(null, null),
@@ -23,11 +23,29 @@ export class CreateProductComponent implements OnInit {
       description: new FormControl(null, null),
       logo: new FormControl(null, null),
       date_release: new FormControl(null, null),
-      date_revision: new FormControl(null, null)
+      date_revision: new FormControl({value: '', disabled: true}, null)
     });
   }
 
+  setDate() {
+    const currentDate = this.productoForm.get('date_release')?.value;
+    const datePipe = new DatePipe('en-US');
+    let date!: Date | null;
+
+    if (currentDate) {
+      date = new Date(currentDate) || null;
+    }
+
+    if ((date instanceof Date)) {
+      date.setFullYear(date.getFullYear() + 1);
+      date.setDate(date.getDate() + 1); // se suma el dia que se perdia en la suma del año
+      this.productoForm.get('date_revision')?.patchValue(datePipe.transform(date, 'yyyy-MM-dd'));
+    }
+
+  }
+
   sendForm() {
+    console.log(this.productoForm.getRawValue());
     if (this.productoForm.valid) {
       console.log(this.productoForm.getRawValue());
     } else {
